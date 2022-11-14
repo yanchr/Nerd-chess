f98bdb16a527656a76554b489868882369504d1c
Janosch Moor <janosch.moor@gmail.com>
2022-11-14 11:39:57 +0100

    knight move done

--- front/js/pieces/knight.js
+++ front/js/pieces/knight.js
@@ -2,5 +2,30 @@ class Knight extends Piece {
     constructor(type, position) {
         super(type, position);
     }
+    validateMove(targetSquare, pieceAtTarget, board) {
+        const deltaX = Math.abs(this.position.x - targetSquare.x);
+        const deltaY = Math.abs(this.position.y - targetSquare.y);
+        if (
+            deltaX+deltaY == 3 && (deltaX == deltaY+1 || deltaX == deltaY-1) &&
+            (!pieceAtTarget || pieceAtTarget.side != this.side)
+        ) {
+            return {isValidMove: true, exitCode: 1};
+        }
+        return {isValidMove: false, exitCode: 0};
+    }
+    moveTo(validationSignature, targetSquare, pieceAtTarget, board) {
+        board.updateEnPassantTargetSquare();
+        if (pieceAtTarget) {
+            pieceAtTarget.isTaken(board);
+        }
+        board.squares[targetSquare.x][targetSquare.y] = this;
+        board.squares[this.position.x][this.position.y] = false;
+
+        this.position.x = targetSquare.x;
+        this.position.y = targetSquare.y;
+    }
+    isTaken(board) {
+        board.squares[this.position.x][this.position.y] = false;
+    }
 
 }
